00160ab5eed55919b1e8392177e10eb0bb03205a
Farbod909 <rafezyfarbod@gmail.com>
2015-09-17 18:59:25 -0700

    Adds full user info and password encryption; makes sure id does not repeat. Better error handling, and new table styles

--- server/User.js
+++ server/User.js
@@ -1,10 +1,51 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt'),
+Schema = mongoose.Schema;
+SALT_WORK_FACTOR = 10;
 
 var userSchema = new Schema({
-  firstname: String,
-  lastname: String
+  id:         { type: Number, required: true, unique: true },
+  username:   { type: String, required: true, unique: true },
+  password:   { type: String, required: true },
+  firstname:  { type: String, required: true },
+  lastname:   { type: String, required: true },
+  position:   { type: String, required: true },
+  email:      { type: String, required: true, unique: true },
+  phone:      { type: Number, required: true, unique: true },
+  created_at: Date,
+  updated_at: Date
 });
+
+userSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 var User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var mongoose = require('mongoose');
+// var bcrypt = require('bcrypt'),
 var functions = [];
 
 //schemas
 var User = require('./User.js');
 
-mongoose.connect('mongodb://localhost:27017/hello1');
+mongoose.connect('mongodb://localhost:27017/expressiment');
 
 function send404(response) {
   response.writeHead(404, {
@@ -27,7 +28,7 @@ function requestHandler(request, response) {
     if (request.method == "GET" && requrl == "/") {
       fs.createReadStream("../website/index.html").pipe(response);
     } else {
-      if (requrl.indexOf(".htm") > -1) {
+      if (requrl.indexOf(".") > -1) {
         fs.readFile("../website" + requrl, function(error, data) {
           if (error) {
             send404(response);
@@ -89,18 +90,76 @@ console.log("Server is now running on port " + port);
 newFunc("createUser", "POST", function(request, response, get, post) {
   var data = parseJSON(post);
 
-  User.create({ firstname: data.firstname, lastname: data.lastname }, function (err, user) {
-    if (err) return handleError(err);
-    console.log(data.firstname + " " + data.lastname + " was saved!");
+  User.find( { $or: [ { username: data.username }, { email: data.email }, { phone: data.phone } ] }, function(err, users){ //see if user exists
+    if( users.length != 0 ){
+      response.end("exists");
+    }else{
+      User.find({id: data.id}, function(err, users){ //see if id exists
+        if (err) {
+          console.error(err);
+        }
+        if(users.length == 0){
+          User.create({
+            id: data.id,
+            username: data.username,
+            password: data.password,
+            firstname: data.firstname,
+            lastname: data.lastname,
+            position: data.position,
+            email: data.email,
+            phone: data.phone
+          }, function(err, user) {
+            if(err){
+              response.end("fail");
+              console.error(err);
+            }else{
+              console.log("User " + data.id + ", " + data.firstname + " " + data.lastname + " was saved!");
+              response.end("success");
+            }
+          });
+        }else{
+          User.create({
+            id: Math.floor(Math.random() * (100000000000 - 10000000000)) + 10000000000, //create new id
+            username: data.username,
+            password: data.password,
+            firstname: data.firstname,
+            lastname: data.lastname,
+            position: data.position,
+            email: data.email,
+            phone: data.phone
+          }, function(err, user) {
+            if(err){
+              response.end("fail");
+              console.error(err);
+            }else{
+              console.log("User " + data.id + ", " + data.firstname + " " + data.lastname + " was saved!");
+              response.end("success");
+            }
+          });
+        }
+      })
+    }
   });
-
-  response.end(JSON.stringify({
-    "firstname": data.firstname,
-    "lastname": data.lastname
-  }));
 });
 newFunc("getUsers", "POST", function(request, response, get, post) {
-  User.find({}, function(err, users){
-    response.end(JSON.stringify(users));
+  User.find({}, function(err, users) {
+    if(err){
+      response.end("fail");
+      console.error(err);
+    }else{
+      response.end(JSON.stringify(users));
+    }
   })
+});
+newFunc("deleteUser", "POST", function(request, response, get, post) {
+  var data = parseJSON(post);
+  User.findOneAndRemove({id: data.id}, function(err){
+    if(err){
+      response.end("fail");
+      console.error(err);
+    }else{
+      console.log("User deleted");
+      response.end("success");
+    }
+  });
 });
