233022c3a155db21b62503d6e9ec2e8d104261f3
Farbod909 <rafezyfarbod@gmail.com>
2015-09-28 15:15:19 -0700

    Changes the way pages are served, adds dynamic subdivision and user pages, adds UI to login, signup and home page, adds invitation accepting for subdivisions

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
-    http = require('http');
+  http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('client-sessions');
+publicDir = require("path").join(__dirname, "../website/public");
 //schemas
 var User = require('./schemas/User.js');
 var Team = require('./schemas/Team.js');
@@ -21,36 +22,55 @@ function send404(response) {
   });
   response.end("404: Page Not Found");
 }
+
 function parseJSON(str) {
   try {
     return JSON.parse(String(str));
   } catch (ex) {}
 }
+
 function createToken(size) {
-	var token = "";
-	for(var i = 0; i < size; i++) {
-		var rand = Math.floor(Math.random() * 62);
-		token += String.fromCharCode(rand + ((rand < 26) ? 97 : ((rand < 52) ? 39 : -4)));
-	}
-	return token;
+  var token = "";
+  for (var i = 0; i < size; i++) {
+    var rand = Math.floor(Math.random() * 62);
+    token += String.fromCharCode(rand + ((rand < 26) ? 97 : ((rand < 52) ? 39 : -4)));
+  }
+  return token;
 }
-function requireLogin (req, res, next) {
+
+function requireLogin(req, res, next) {
   if (!req.user) {
     res.end("fail");
   } else {
     next();
   }
-};
-function searchForValueInObjectsArray(value, array){
-    for (var i=0; i < array.length; i++) {
-        if (array[i].id === value) {
-            return array[i];
-        }
-    }
 }
 
-//for parsing bodies ;)
-app.use( bodyParser.json() );
+function userNotFound(response) {
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.end("User not found");
+}
+
+function subdivisionNotFound(response) {
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.end("Subdivision not found");
+}
+
+
+String.prototype.contains = function(arg) {
+  return this.indexOf(arg) > -1;
+};
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Oops, something went wrong!');
+});
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -66,7 +86,9 @@ app.use(session({
 }));
 app.use(function(req, res, next) {
   if (req.session && req.session.user) {
-    User.findOne({ username: req.session.user.username }, function(err, user) {
+    User.findOne({
+      username: req.session.user.username
+    }, function(err, user) {
       if (user) {
         req.user = user;
         delete req.user.password;
@@ -78,69 +100,162 @@ app.use(function(req, res, next) {
     next();
   }
 });
-
-app.use(function(request, response, next) {
-  var requrl = url.parse(request.url).pathname;
-  var query = url.parse(request.url).query;
-  var get = qs.parse(query);
-
-  if (requrl.indexOf("/f/") == -1) {
-    if(request.method == "GET" && (requrl == "/login" || requrl == "/login.html")){
-      fs.createReadStream("../website/login.html").pipe(response);
-    }else if(request.method == "GET" && (requrl == "/signup" || requrl == "/signup.html")){
-      fs.createReadStream("../website/signup.html").pipe(response);
-    }else if(request.method == "GET" && (requrl == "/void" || requrl == "/void.html")){
-      if(request.user.teams.length != 0){
-        response.redirect("/");
-      }else{
-        fs.createReadStream("../website/void.html").pipe(response);
-      }
-    }else{
-      if(request.user){
-        if (request.method == "GET" && request.user.teams.length == 0){
-          fs.createReadStream("../website/void.html").pipe(response);
-        }else{
-          if (request.method == "GET" && requrl == "/") {
-            fs.createReadStream("../website/index.html").pipe(response);
-          } else {
-            if (requrl.indexOf(".") > -1) {
-              fs.readFile("../website" + requrl, function(error, data) {
-                if (error) {
-                  send404(response);
-                } else {
-                  response.end(data);
-                }
-              });
-            } else {
-              fs.readFile("../website" + requrl + ".html", function(error, data) {
-                if (error) {
-                  send404(response);
-                } else {
-                  response.end(data);
-                }
-              });
-            }
-          }
+app.use(function(req, res, next) {
+  if (req.path.indexOf('.') === -1) {
+    var file = publicDir + req.path + '.html';
+    fs.exists(file, function(exists) {
+      if (exists)
+        req.url += '.html';
+      next();
+    });
+  } else
+    next();
+});
+app.use(function(req, res, next) {
+  if (req.method == "GET") {
+    if (req.url.contains("/css") || req.url.contains("/js") || req.url.contains("/img")) {
+      next();
+    } else if (req.url == "/login.html") {
+      next();
+    } else if (req.url == "/signup.html") {
+      next();
+    } else if (req.url == "/void.html") {
+      if (req.user) {
+        if (req.user.teams.length > 0) {
+          res.redirect("/");
+        } else {
+          next();
         }
-      }else{
-        response.redirect("/login");
+      } else {
+        res.redirect("/");
+      }
+    } else {
+      if (req.user) {
+        if (req.user.teams.length > 0) {
+          next();
+        } else {
+          res.redirect("/void");
+        }
+      } else {
+        res.redirect("/login");
       }
     }
-  }else{
+  } else {
     next();
   }
 });
+app.use(express.static(publicDir));
 
-app.post("/f/createUser", function(req, res){
-  User.find( { $or: [ { username: req.body.username }, { email: req.body.email }, { phone: req.body.phone } ] }, function(err, users){ //see if user exists
-    if( users.length != 0 ){
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/../website');
+
+app.get("/", function(req, res) {
+  fs.createReadStream("../website/public/index.html").pipe(res);
+});
+app.get("/u/:id", function(req, res) {
+  User.findOne({
+    id: req.params.id,
+    teams: {
+      "$in": req.user.teams
+    }
+  }, function(err, user) {
+    if (err) {
+      send404(res);
+    } else {
+      if (user) {
+        res.render('user', user);
+      } else {
+        userNotFound(res);
+      }
+    }
+  })
+});
+app.get("/s/:id", function(req, res) {
+  var joined = false;
+  Subdivision.findOne({
+    id: req.params.id,
+    team: {
+      "$in": req.user.teams
+    }
+  }, function(err, subdivision) {
+    if (err) {
+      send404(res);
+    } else {
+      if (subdivision) {
+        User.find({
+          subdivisions: {
+            $elemMatch: {
+              id: subdivision.id,
+              accepted: true
+            }
+          }
+        }, function(err, users) {
+          if (err) {
+            send404(res);
+          } else {
+            if (subdivision.type == "private") {
+              for (var i = 0; i < users.length; i++) {
+                if (users[i].id == req.user.id) {
+                  res.render('subdivision', {
+                    name: subdivision.name,
+                    type: subdivision.type,
+                    team: subdivision.team,
+                    joined: true,
+                    members: users
+                  });
+                  break;
+                }
+              }
+              res.end("nothing to see here.");
+            } else if (subdivision.type == "public") {
+              for (var i = 0; i < users.length; i++) {
+                if (users[i].id == req.user.id) {
+                  joined = true
+                  break;
+                }
+              }
+
+              res.render('subdivision', {
+                name: subdivision.name,
+                type: subdivision.type,
+                team: subdivision.team,
+                joined: joined,
+                members: users
+              });
+            }
+          }
+        })
+      } else {
+        subdivisionNotFound(res);
+      }
+    }
+  })
+});
+
+app.get('*', function(req, res) {
+  send404(res);
+});
+
+app.post("/f/createUser", function(req, res) {
+  User.find({
+    $or: [{
+      username: req.body.username
+    }, {
+      email: req.body.email
+    }, {
+      phone: req.body.phone
+    }]
+  }, function(err, users) { //see if user exists
+    if (users.length != 0) {
       res.end("exists");
-    }else{
-      User.find({id: req.body.id}, function(err, users){ //see if id exists
+    } else {
+      User.find({
+        id: req.body.id
+      }, function(err, users) { //see if id exists
         if (err) {
           console.error(err);
         }
-        if(users.length == 0){
+        if (users.length == 0) {
           User.create({
             id: req.body.id,
             username: req.body.username,
@@ -150,15 +265,15 @@ app.post("/f/createUser", function(req, res){
             email: req.body.email,
             phone: req.body.phone
           }, function(err, user) {
-            if(err){
+            if (err) {
               res.end("fail");
               console.error(err);
-            }else{
+            } else {
               console.log("User " + req.body.id + ", " + req.body.firstname + " " + req.body.lastname + " was saved!");
               res.end("success");
             }
           });
-        }else{
+        } else {
           User.create({
             id: Math.floor(Math.random() * (100000000000 - 10000000000)) + 10000000000, //create new id
             username: req.body.username,
@@ -168,10 +283,10 @@ app.post("/f/createUser", function(req, res){
             email: req.body.email,
             phone: req.body.phone
           }, function(err, user) {
-            if(err){
+            if (err) {
               res.end("fail");
               console.error(err);
-            }else{
+            } else {
               console.log("User " + req.body.id + ", " + req.body.firstname + " " + req.body.lastname + " was saved!");
               res.end("success");
             }
@@ -181,83 +296,93 @@ app.post("/f/createUser", function(req, res){
     }
   });
 });
-app.post("/f/getUsersInTeam", function(req, res){
-  User.find({teams: req.body.team_id }, function(err, users) {
-    if(err){
+app.post("/f/getUsersInTeam", function(req, res) {
+  User.find({
+    teams: req.body.team_id
+  }, function(err, users) {
+    if (err) {
       console.error(err);
       res.end("fail");
-    }else{
+    } else {
       res.end(JSON.stringify(users));
     }
   })
 });
-app.post("/f/deleteUser", function(req, res){
-  User.findOneAndRemove({id: req.body.id}, function(err){
-    if(err){
+app.post("/f/deleteUser", function(req, res) {
+  User.findOneAndRemove({
+    id: req.body.id
+  }, function(err) {
+    if (err) {
       res.end("fail");
       console.error(err);
-    }else{
+    } else {
       console.log("User deleted");
       res.end("success");
     }
   });
 });
-app.post("/f/login", function(req, res){
-  User.findOne({username: req.body.username}, function(err, user){
-    if(user){
-      user.comparePassword(req.body.password, function(err, isMatch){
-        if(err){
+app.post("/f/login", function(req, res) {
+  User.findOne({
+    username: req.body.username
+  }, function(err, user) {
+    if (user) {
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (err) {
           console.error(err);
-        }else{
-          if(isMatch){
+        } else {
+          if (isMatch) {
             req.session.user = user;
             res.end(JSON.stringify(user));
-          }else{
+          } else {
             res.end("inc/password");
           }
         }
       })
-    }else{
+    } else {
       res.end("inc/username")
     }
   });
 });
-app.post("/f/logout", function(req, res){
+app.post("/f/logout", function(req, res) {
   req.session.reset();
   res.end("success");
 });
-app.post("/f/createTeam", function(req, res){
+app.post("/f/createTeam", function(req, res) {
   Team.create({
     id: req.body.id,
     name: req.body.name,
     number: req.body.number
-  }, function(err, team){
-    if(err){
+  }, function(err, team) {
+    if (err) {
       console.error(err);
       res.end("fail");
-    }else{
+    } else {
       res.end("success");
     }
   });
 });
-app.post("/f/joinTeam", function(req, res){
-  Team.findOne({id: req.body.team_id}, function(err, team){
-    if(err){
+app.post("/f/joinTeam", function(req, res) {
+  Team.findOne({
+    id: req.body.team_id
+  }, function(err, team) {
+    if (err) {
       console.error(err);
       res.end("fail");
-    }else{
-      if(team){
-        User.findOne({id: req.body.user_id}, function(err, user){
-          if(err){
+    } else {
+      if (team) {
+        User.findOne({
+          id: req.body.user_id
+        }, function(err, user) {
+          if (err) {
             console.error(err);
             res.end("fail")
-          }else{
+          } else {
             user.teams.push(req.body.team_id);
-            user.save(function(err){
-              if(err){
+            user.save(function(err) {
+              if (err) {
                 console.error(err);
                 res.end("fail");
-              }else{
+              } else {
                 res.end("success");
               }
             });
@@ -267,71 +392,125 @@ app.post("/f/joinTeam", function(req, res){
     }
   });
 });
-app.post("/f/createSubdivision", function(req, res){
+app.post("/f/createSubdivision", function(req, res) {
   Subdivision.create({
     id: Math.floor(Math.random() * (100000000000 - 10000000000)) + 10000000000,
     name: req.body.name,
     type: req.body.type,
     team: req.body.team_id
-  }, function(err, subdivision){
-    if(err){
+  }, function(err, subdivision) {
+    if (err) {
       console.error(err);
       res.end("fail");
-    }else{
+    } else {
       res.end(subdivision.id);
     }
   });
 });
-app.post("/f/inviteToSubdivision", function(req, res){
-  Subdivision.findOne({id: req.body.subdivision_id}, function(err, subdivision){
-    if(err){
+app.post("/f/inviteToSubdivision", function(req, res) {
+  Subdivision.findOne({
+    id: req.body.subdivision_id
+  }, function(err, subdivision) {
+    if (err) {
       console.error(err);
       res.end("fail");
-    }else{
-      if(subdivision){
-        User.findOne({id: req.body.user_id}, function(err, user){
-          if(err){
+    } else {
+      if (subdivision) {
+        User.findOne({
+          id: req.body.user_id
+        }, function(err, user) {
+          if (err) {
             console.error(err);
             res.end("fail");
-          }else{
-            // searchForValueInObjectsArray(req.body.team_id, user.teams).subdivisions.push(req.body.subdivision_id);
-            // for (var i=0; i < user.teams.length; i++) {
-            //   if (user.teams[i].id == req.body.team_id) {
-            //     user.teams[i].subdivisions.push(req.body.subdivision_id.toString());
-            //     console.log(user);
-            //     user.save(function(err){
-            //       if(err){
-            //         console.error(err);
-            //         res.end("fail");
-            //       }else{
-            //         res.end("success")
-            //       }
-            //     });
-            //     break;
-            //   }
-            // }
-            if(user){
-              user.subdivisions.push({id: req.body.subdivision_id, team: req.body.team_id});
-              user.save(function(err){
-                if(err){
+          } else {
+            if (user) {
+              user.subdivisions.push({
+                id: req.body.subdivision_id,
+                team: req.body.team_id,
+                accepted: false
+              });
+              user.save(function(err) {
+                if (err) {
                   console.error(err);
                   res.end("fail");
-                }else{
+                } else {
                   res.end("success");
                 }
               })
-            }else{
+            } else {
               res.end("fail");
             }
           }
         });
-      }else{
+      } else {
         res.end("fail");
       }
     }
   });
 });
+app.post("/f/getPublicSubdivisions", function(req, res) {
+  Subdivision.find({team: req.body.team_id, type: "public"}, function(err, subdivisions) {
+    if (err) {
+      console.error(err);
+      res.end("fail")
+    } else {
+      res.end(JSON.stringify(subdivisions));
+    }
+  })
+});
+app.post("/f/getAllSubdivisionsForUserInTeam", function(req, res) {
+  var userSubdivisionIds = []
+  for(var i = 0; i < req.user.subdivisions.length; i++){
+    if(req.user.subdivisions[i].accepted == true){
+      userSubdivisionIds.push(req.user.subdivisions[i].id);
+    }
+  }
+  Subdivision.find({id: { "$in": userSubdivisionIds } }, function(err, subdivisions){
+    res.end( JSON.stringify(subdivisions.map(function(subdivision) {return {name: subdivision.name, id: subdivision.id} })) );
+  })
+});
 
+app.post("/f/loadSubdivisionInvitations", function(req, res) {
+  var invitedSubdivisions = [];
+  if(req.user.subdivisions.length > 0){
+    for (var i = 0; i < req.user.subdivisions.length; i++) {
+      if (req.user.subdivisions[i].accepted == false && req.user.subdivisions[i].team == req.body.team_id){
+        Subdivision.findOne({id: req.user.subdivisions[i].id, team: req.body.team_id}, function(err, subdivision){
+          if(err){
+            console.error(err);
+            res.end("fail");
+          }else{
+            if(subdivision){
+              invitedSubdivisions.push({
+                name: subdivision.name,
+                id: subdivision.id
+              });
+            }
+          }
+        })
+      }
+      if(i == req.user.subdivisions.length - 1){
+        setTimeout(function(){
+          res.end(JSON.stringify(invitedSubdivisions));
+        }, 100) //TODO: FIND A BETTER WAY TO DO THIS
+      }
+    }
+  }else{
+    res.end("[]");
+  }
+})
+app.post("/f/acceptSubdivisionInvitation", function(req, res){
+  User.update({id: req.user.id, 'subdivisions.id': req.body.subdivision_id}, {'$set': {
+    'subdivisions.$.accepted': true
+  }}, function(err){
+    if(err){
+      console.error(err);
+      res.end("fail");
+    }else{
+      res.end("success");
+    }
+  });
+});
 
 var port = process.argv[2] || 8080;
 app.listen(port);
---
+++ website/public/js/main.js
@@ -0,0 +1,113 @@
+function getUsername(){
+  return localStorage.username || sessionStorage.username;
+}
+function getToken(){
+  return localStorage.token || sessionStorage.token;
+}
+function getId(){
+  return localStorage.id || sessionStorage.id;
+}
+function getTeams(){
+  return JSON.parse(localStorage.teams) || JSON.parse(sessionStorage.teams);
+}
+function removeFromStorage(key){
+  if (localStorage.getItem(key) != null){
+    localStorage.removeItem(key);
+  }else if(sessionStorage.getItem(key) != null){
+    sessionStorage.removeItem(key);
+  }else{
+    console.log("item was not found in storage");
+  }
+}
+$(document).ready(function(){
+  $('#name_link').html(localStorage.firstname);
+
+  var fade_speed = 200;
+  $('#notif_button').click(function() {
+      $('#notif_drop').fadeIn(fade_speed).removeClass("hidden")
+      $('.triangle_thing_notif').fadeIn(fade_speed).removeClass("hidden")
+      $('#darken').fadeIn(fade_speed).removeClass("hidden")
+      $('#notif_button').addClass('active');
+      $('#darken').click(function() {
+          $(this).fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('#notif_drop').fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('.triangle_thing_notif').fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('#notif_button').removeClass('active');
+      });
+  });
+  $('.profile_id').click(function() {
+      $('#prof_drop').fadeIn(fade_speed).removeClass("hidden");
+      $('.triangle_thing_prof').fadeIn(fade_speed).removeClass("hidden");
+      $('#darken').fadeIn(fade_speed).removeClass("hidden");
+      $('#darken').click(function() {
+          $(this).fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('#prof_drop').fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('.triangle_thing_prof').fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+      });
+  });
+  $('.profile_name').click(function() {
+      $('#prof_drop').fadeIn(fade_speed).removeClass("hidden");
+      $('.triangle_thing_prof').fadeIn(fade_speed).removeClass("hidden");
+      $('#darken').fadeIn(fade_speed).removeClass("hidden");
+      $('#darken').click(function() {
+          $(this).fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('#prof_drop').fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+          $('.triangle_thing_prof').fadeOut(fade_speed, function() {
+              $(this).addClass("hidden")
+          });
+      });
+  });
+  $("#logout_button").click(function(){
+    $.post("/f/logout", JSON.stringify({"id": getId(), "token": getToken()}), function(responseText){
+      if(responseText == "success"){
+        removeFromStorage("id");
+        removeFromStorage("username");
+        removeFromStorage("firstname");
+        removeFromStorage("lastname");
+        removeFromStorage("email");
+        removeFromStorage("phone");
+        removeFromStorage("teams");
+        removeFromStorage("c_team");
+        location="login";
+      }
+    });
+  });
+  $("#view_prof_button").click(function(){
+    location="u/"+localStorage.id;
+  });
+  $("#view_prof_button_ejs").click(function(){
+    location="../u/"+localStorage.id;
+  });
+
+  $("#aboutus_link").click(function(){
+    location="about";
+  });
+  $(".menu").click(function() {
+    if ($(".nav_dropdown").css("top").indexOf("-") != -1) {
+        $(".nav_dropdown").velocity({
+            top: "40px"
+        }, 200);
+    } else {
+        $(".nav_dropdown").velocity({
+            top: "-215px"
+        }, 200);
+    }
+  });
+
+});
