7cd9e00ff47dd2655441ca6b58ae6596dbaf83bb
voidpigeon <benheller42@gmail.com>
2016-04-17 16:21:42 -0700

    Finish promisifying teams.js

--- server/teams.js
+++ server/teams.js
@@ -34,199 +34,136 @@ module.exports = function(app, util, schemas) {
 		}
 	}));
 
-	app.post("/f/getUsersInTeam", requireLogin, function(req, res) {
-		User.find({
-			teams: {$elemMatch: {id: req.user.current_team.id }}
-		}, "-password", function(err, users) {
-			if (err) {
-				console.error(err);
-				res.end("fail");
-			} else {
-				res.end(JSON.stringify(users));
+	app.post("/f/getUsersInTeam", requireLogin, Promise.coroutine(function*(req, res) {
+		try {
+
+			let users = yield User.find({
+				teams: {$elemMatch: {id: req.user.current_team.id }}
+			});
+
+			res.end(JSON.stringify(users));
+
+		} catch (err) {
+			console.error(err);
+			res.end("fail");
+		}
+	}));
+
+	app.post("/f/createTeam", requireLogin, Promise.coroutine(function*(req, res) {
+		try {
+
+			let teams = yield Team.find({id: req.body.id});
+
+			if (teams.length != 0) {
+				return res.end("fail");
 			}
-		})
-	});
-	app.post("/f/createTeam", requireLogin, function(req, res) {
-		Team.find({id: req.body.id}, function(err, teams) {
-			if (err) {
-				console.error(err);
-				res.end("fail");
-			} else {
-				if (teams.length == 0) {
-					Team.create({
-						id: req.body.id,
-						name: req.body.name,
-						number: req.body.number
-					}, function(err, team) {
-						if (err) {
-							console.error(err);
-							res.end("fail");
-						} else {
-							Folder.create({
-								name: "Team Files",
-								team: team.id,
-								entireTeam: true,
-								creator: req.user._id,
-								defaultFolder: true
-							}, function(err, folder) {
-								if (err) {
-									console.error(err);
-									res.end("fail");
-								} else {
-									res.end("success");
-								}
-							});
-						}
-					});
-				} else {
-					res.end("fail");
-				}
+
+			let team = yield Team.create({
+				id: req.body.id,
+				name: req.body.name,
+				number: req.body.number
+			});
+
+			let folder = yield Folder.create({
+				name: "Team Files",
+				team: team.id,
+				entireTeam: true,
+				creator: req.user._id,
+				defaultFolder: true
+			});
+
+			res.end("success");
+
+		} catch (err) {
+			console.error(err);
+			res.end("fail");
+		}
+	}));
+
+	app.post("/f/joinTeam", requireLogin, Promise.coroutine(function*(req, res) {
+		try {
+
+			let team = yield Team.findOne({id: req.body.team_id});
+			
+			if (!team) {
+				return res.end("no such team");
 			}
-		});
-	});
-	app.post("/f/joinTeam", requireLogin, function(req, res) {
-		Team.findOne({
-			id: req.body.team_id
-		}, function(err, team) {
-			if (err) {
-				console.error(err);
-				res.end("fail");
-			} else {
-				if (team) {
-					User.findOne({
-						_id: req.user._id
-					}, function(err, user) {
-						if (err) {
-							console.error(err);
-							res.end("fail")
-						} else {
-							if (user) {
-								if ( user.bannedFromTeams.length > 0 && user.bannedFromTeams.indexOf(req.body.team_id) > -1 ) {
-									res.end("banned");
-								} else if (user.teams.length > 0) {
-									User.find({teams: {$elemMatch: {"id": req.body.team_id} } }, function(err, users) {
-										if (err) {
-											console.error(err);
-											res.end("fail");
-										} else {
-											if (users.length > 0) {
-												user.teams.push({id: req.body.team_id, position: "member"});
-											} else {
-												user.teams.push({id: req.body.team_id, position: "admin"});
-											}
-											AttendanceHandler.update({ entireTeam: true, event_date: { $gte: new Date() } }, {
-												"$push": {
-													"attendees": {user: req.user._id, status: "absent"}
-												}
-											}, function(err, model) {
-												if (err) {
-													console.error(err);
-													res.end("fail");
-												} else {
-													user.save(function(err) {
-														if (err) {
-															console.error(err);
-															res.end("fail");
-														} else {
-															Folder.create({
-																name: "Personal Files",
-																team: req.body.team_id,
-																userMembers: req.user._id,
-																creator: req.user._id,
-																defaultFolder: true
-															}, function(err, folder) {
-																if (err) {
-																	console.error(err);
-																	res.end("fail");
-																} else {
-																	res.end("success");
-																}
-															});
-														}
-													});
-												}
-											})
-										}
-									});
-								} else {
-									User.find({teams: {$elemMatch: {"id": req.body.team_id} } }, function(err, users) {
-										if (err) {
-											console.error(err);
-											res.end("fail");
-										} else {
-											if (users.length > 0) {
-												user.teams.push({id: req.body.team_id, position: "member"});
-												user.current_team = {id: req.body.team_id, position: "member"};
-											} else {
-												user.teams.push({id: req.body.team_id, position: "admin"});
-												user.current_team = {id: req.body.team_id, position: "admin"};
-											}
-											AttendanceHandler.update({ entireTeam: true, event_date: { $gte: new Date() } }, {
-												"$push": {
-													"attendees": {user: req.user._id, status: "absent"}
-												}
-											}, function(err, model) {
-												if (err) {
-													console.error(err);
-													res.end("fail");
-												} else {
-													user.save(function(err) {
-														if (err) {
-															console.error(err);
-															res.end("fail");
-														} else {
-															Folder.create({
-																name: "Personal Files",
-																team: req.body.team_id,
-																userMembers: req.user._id,
-																creator: req.user._id,
-																defaultFolder: true
-															}, function(err, folder) {
-																if (err) {
-																	console.error(err);
-																	res.end("fail");
-																} else {
-																	res.end("success");
-																}
-															});
-														}
-													});
-												}
-											})
-										}
-									});
-								}
-							}
-						}
-					});
-				} else {
-					res.end("no such team");
-				}
+
+			let user = yield User.findOne({_id: req.user._id});
+
+			if (!user) {
+				return res.end("fail");
 			}
-		});
-	});
-	app.post("/f/getTeamNum", requireLogin, function(req, res) {
-		Team.findOne({id: req.user.current_team.id}, function(err, team) {
-			if (err) {
-				console.error(err);
-				res.end("fail");
-			} else {
-				res.end(String(team.number));
+
+			if ( user.bannedFromTeams.length > 0 && user.bannedFromTeams.indexOf(req.body.team_id) > -1 ) {
+				return res.end("banned");
 			}
-		})
-	});
-	app.post("/f/isTeamOnMorTeam", requireLogin, function(req, res) {
-		Team.find({number: parseInt(req.body.teamNum)}, function(err, teams) {
-			if (err) {
-				console.error(err);
-				res.end("fail");
-			} else {
-				if (teams.length > 0) {
-					res.json(teams[0]);
-				} else {
-					res.end("false");
-				}
+
+			let users = yield User.find({teams: {$elemMatch: {"id": req.body.team_id}}});
+
+			let newTeam = {
+				id: req.body.team_id,
+				position: users.length == 0 ? "admin" : "member"
+			};
+			user.teams.push(newTeam);
+			if (user.teams.length == 0) {
+				user.current_team = newTeam;
 			}
-		});
-	});
+
+			// TODO: does this do what it is supposed to do?
+			yield AttendanceHandler.update({
+				entireTeam: true,
+				event_date: { $gte: new Date() }
+			}, {"$push": {
+				"attendees": {user: req.user._id, status: "absent"}
+			}});
+			
+			yield user.save();
+			
+			yield Folder.create({
+				name: "Personal Files",
+				team: req.body.team_id,
+				userMembers: req.user._id,
+				creator: req.user._id,
+				defaultFolder: true
+			});
+
+			res.end("success");
+
+		} catch (err) {
+			console.log(err);
+			res.end("fail");
+		}
+	}));
+
+	app.post("/f/getTeamNum", requireLogin, Promise.coroutine(function*(req, res) {
+		try {
+
+			let team = yield Team.findOne({id: req.user.current_team.id});
+
+			res.end(String(team.number));
+
+		} catch (err) {
+			console.error(err);
+			res.end("fail");
+		}
+	}));
+
+	app.post("/f/isTeamOnMorTeam", requireLogin, Promise.coroutine(function*(req, res) {
+		try {
+			
+			let teams = Team.find({number: parseInt(req.body.teamNum)});
+
+			if (teams.length > 0) {
+				res.json(teams[0]); // TODO: should this just be "true" instead?
+			} else {
+				res.end("false");
+			}
+
+		} catch (err) {
+			console.error(err);
+			res.end("fail");
+		}
+	}));
+
 };
