2e58179377bdd733fb68f57b6f664cde6380dec9
voidpigeon <benheller42@gmail.com>
2016-04-30 16:18:35 -0700

    Add support for multiple chat clients per user
    Before, there were problems with chat if multiple tabs were opened. TODO: If a user sends a message, show the message they sent on other clients that they are logged in to.

--- server/sio.js
+++ server/sio.js
@@ -11,106 +11,118 @@ module.exports = function(io, util, schemas) {
 	let online_clients = {};
 	io.on("connection", Promise.coroutine(function*(socket) {
 		let sess = socket.request.session.user;
-		if (sess && online_clients[sess._id] == undefined) {
-			let userSubdivisionIds = sess.subdivisions.map(function(subdivision) {
-				if (subdivision.accepted == true) {
-					return new ObjectId(subdivision._id);
-				}
-			});
+		if (sess) {
+			let userSubdivisionIds = util.activeSubdivisionIds(sess.subdivisions);
 
 			try {
 
-				let chats = yield Chat.find({
-					team: sess.current_team.id,
-					$or: [
-						{
-							userMembers: new ObjectId(sess._id)
-						},
-						{
-							subdivisionMembers: {
-								"$in": userSubdivisionIds
+				if (!(sess._id in online_clients)) {
+
+					let chats = yield Chat.find({
+						team: sess.current_team.id,
+						$or: [
+							{ userMembers: new ObjectId(sess._id) },
+							{ subdivisionMembers: { "$in": userSubdivisionIds } }
+						]
+					}, { _id: 1 }).exec();
+
+					let chatIds = chats.map(chat => chat._id.toString());
+
+					for ( let user_id in online_clients ) {
+						if ( online_clients[user_id].chats.hasAnythingFrom( chatIds ) ) {
+							for (let sock of online_clients[user_id].sockets) {
+								io.to(sock ).emit("joined", {_id: sess._id});
 							}
 						}
-					]
-				}, {
-					_id: 1
-				}).exec();
-
-				let chatIds = chats.map(function(chat) { return chat._id.toString() });
-				online_clients[sess._id] = {socket: socket.id, chats: chatIds};
-				for ( let user_id in online_clients ) {
-					if ( online_clients[user_id].chats.hasAnythingFrom( online_clients[sess._id].chats ) && user_id != sess._id  ) {
-						io.to( online_clients[user_id].socket ).emit("joined", {_id: sess._id});
 					}
+					online_clients[sess._id] = {
+						chats: chatIds,
+						sockets: []
+					};
 				}
+				online_clients[sess._id].sockets.push(socket.id);
 
 			} catch (err) {
 				console.error(err);
-				// res.end("fail");
-			}
-		} else if (sess) {
-			for ( let user_id in online_clients ) {
-				if ( online_clients[user_id].chats.hasAnythingFrom( online_clients[sess._id].chats ) && user_id != sess._id  ) {
-					io.to( online_clients[user_id].socket ).emit("joined", {_id: sess._id});
-				}
 			}
 		}
 
 		socket.on("disconnect", function() {
-			for ( let user_id in online_clients ) {
-				if (sess && online_clients[sess._id]) { // TODO: sometimes online_clients[sess._id] doesnt exist (maybe because it takes time for the mongo query to execute and add user chats to the online_clients object at the sess._id index)
-					if ( online_clients[user_id].chats.hasAnythingFrom( online_clients[sess._id].chats ) && user_id != sess._id  ) {
-						io.to( online_clients[user_id].socket ).emit("left", {_id: sess._id});
+			if (!sess || !(sess._id in online_clients)) {
+				// TODO: sometimes online_clients[sess._id] doesnt exist
+				// (maybe because it takes time for the mongo query to execute
+				// and add user chats to the online_clients object at the sess._id index)
+				return;
+			}
+
+			let index = online_clients[sess._id].sockets.indexOf(socket.id);
+			if (index != -1) {
+				online_clients[sess._id].sockets.splice(index, 1); // remove the socket from the list of sockets for the user
+
+				if (online_clients[sess._id].sockets.length == 0) { // if no clients remain for the user
+
+					let chatIds = online_clients[sess._id].chats;
+					delete online_clients[sess._id]; // remove from online clients
+
+					for ( let user_id in online_clients ) { // notify other clients that they went offline
+						if ( online_clients[user_id].chats.hasAnythingFrom(chatIds)) { // if they have any chats in common
+							for (let sock of online_clients[user_id].sockets) {
+								io.to(sock).emit("left", {_id: sess._id});
+							}
+						}
 					}
+
 				}
 			}
-			if (sess) {
-				delete online_clients[sess._id];
-			}
 		});
+
+		// TODO: if a user has multiple clients and sends a message, display sent message on all clients
 
 		socket.on("message", function(msg) {
 			msg.content = util.normalizeDisplayedText(msg.content);
 			for ( let user_id in online_clients ) {
+				if (user_id == sess._id) {
+					continue; // don't send messages to the user that sent them
+				}
+				console.log(online_clients[user_id].sockets.length);
 				let client_chats = online_clients[user_id].chats.map(chat_id => chat_id.toString());
-				if ( ~client_chats.indexOf( msg.chat_id ) && user_id != sess._id ) {
+				if ( client_chats.indexOf( msg.chat_id ) != -1 ) { // if the user is part of the chat the message was sent to
+					let message = {
+						chat_id: msg.chat_id,
+						author_id: sess._id,
+						author_fn: sess.firstname,
+						author_ln: sess.lastname,
+						author_profpicpath: sess.profpicpath,
+						content: msg.content,
+						timestamp: new Date()
+					};
 					if (msg.type == "private") {
-						io.to( online_clients[user_id].socket ).emit("message", {
-							chat_id: msg.chat_id,
-							author_id: sess._id,
-							author_fn: sess.firstname,
-							author_ln: sess.lastname,
-							author_profpicpath: sess.profpicpath,
-							content: msg.content,
-							timestamp: new Date(),
-							type: "private"
-						});
+						message.type = "private";
 					} else {
-						io.to( online_clients[user_id].socket ).emit("message", {
-							chat_id: msg.chat_id,
-							author_id: sess._id,
-							author_fn: sess.firstname,
-							author_ln: sess.lastname,
-							author_profpicpath: sess.profpicpath,
-							content: msg.content,
-							timestamp: new Date(),
-							chat_name: msg.chat_name,
-							type: "group"
-						});
+						message.type = "group";
+						message.chat_name = msg.chat_name;
+					}
+					for (let sock of online_clients[user_id].sockets) {
+						io.to(sock).emit("message", message);
 					}
 				}
 			}
 		});
 
 		socket.on("get clients", function() {
-			socket.emit("get clients", online_clients)
+			var obj = {};
+			for (let key in online_clients) {
+				obj[key] = 0; // yeah... don't want to break mobile quite yet
+				// TODO: break mobile and make this sane
+			}
+			socket.emit("get clients", obj);
 		});
 
 		socket.on("new chat", Promise.coroutine(function*(data) {
 			if (data.type == "private") {
 				if ( online_clients[data.receiver] ) {
 					online_clients[data.receiver].chats.push( data.chat_id );
-					io.to( online_clients[ data.receiver ].socket ).emit("new chat", {
+					io.to( online_clients[ data.receiver ].sockets ).emit("new chat", {
 						type: "private",
 						chat_id: data.chat_id,
 						user_id: sess._id,
@@ -133,17 +145,18 @@ module.exports = function(io, util, schemas) {
 						]
 					});
 
-					for (let i = 0; i < users.length; i++) {
-						if ( online_clients[ users[i]._id.toString() ] != undefined ) {
-							online_clients[users[i]._id.toString()].chats.push( data.chat_id );
-							io.to( online_clients[ users[i]._id.toString() ].socket ).emit("new chat", {
+					for (let user of users) {
+						let userId = user._id.toString();
+						if ( online_clients[userId] != undefined ) {
+							online_clients[userId].chats.push( data.chat_id );
+							io.to( online_clients[userId].sockets ).emit("new chat", {
 								type: "group",
 								user_id: sess._id,
 								userMembers: data.userMembers,
 								subdivisionMembers: data.subdivisionMembers,
 								name: data.name,
 								chat_id: data.chat_id
-							})
+							});
 						}
 					}
 
@@ -157,7 +170,9 @@ module.exports = function(io, util, schemas) {
 		socket.on("start typing", function(data) {
 			for ( let user_id in online_clients ) {
 				if ( ~online_clients[user_id].chats.indexOf( data.chat_id ) && user_id != sess._id ) {
-					io.to( online_clients[user_id].socket ).emit("start typing", data)
+					for (let sock of online_clients[user_id].sockets) {
+						io.to(sock).emit("start typing", data);
+					}
 				}
 			}
 		});
@@ -165,7 +180,9 @@ module.exports = function(io, util, schemas) {
 		socket.on("stop typing", function(data) {
 			for ( let user_id in online_clients ) {
 				if ( ~online_clients[user_id].chats.indexOf( data.chat_id ) && user_id != sess._id ) {
-					io.to( online_clients[user_id].socket ).emit("stop typing", data)
+					for (let sock of online_clients[user_id].sockets) {
+						io.to(sock).emit("stop typing", data);
+					}
 				}
 			}
 		});
