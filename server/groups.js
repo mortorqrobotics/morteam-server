417dec8a76f7ccab434406d4b308f97b8f003df7
afuous <benheller42@gmail.com>
2016-06-26 18:06:54 -0700

    Remove subdivisions.js, add groups.js

---
+++ server/groups.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = function(imports) {
+
+	let express = imports.modules.express;
+	let Promise = imports.modules.Promise;
+	let util = imports.util;
+
+	let requireLogin = util.requireLogin;
+
+	let router = express.Router();
+
+	return router;
+
+};
