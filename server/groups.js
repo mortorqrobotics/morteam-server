b83a3b651f2ba937c0de3487e08a139887d92a3b
E-A-A <ev34an@gmail.com>
2016-06-29 00:58:54 +0000

    Add create group route

--- server/groups.js
+++ server/groups.js
@@ -5,10 +5,23 @@ module.exports = function(imports) {
     let express = imports.modules.express;
     let Promise = imports.modules.Promise;
     let util = imports.util;
-
+    let Group = imports.models.NormalGroup;
     let requireLogin = util.requireLogin;
-
+    
     let router = express.Router();
+
+    router.post("groups", Promise.coroutine(function*(req, res){
+            let group = {
+                users: req.body.users,
+                groups: req.body.groups
+            }
+            try{
+                group = yield Group.create(group);
+            } catch(err){
+                console.log(err);
+                res.end("fail");
+            }
+        }));
 
     return router;
 
