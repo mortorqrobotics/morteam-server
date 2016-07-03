18222eb22db77fe50a8abd3f7f1751ce8f7d19ec
E-A-A <ev34an@gmail.com>
2016-07-03 22:26:59 +0000

    remove team from tasks

--- server/tasks.js
+++ server/tasks.js
@@ -24,7 +24,6 @@ module.exports = function(imports) {
 
         let task = {
             name: req.body.task_name,
-            team: req.user.team,
             for: req.params.userId, // why a reserved word :/
             due_date: req.body.due_date,
             creator: req.user._id,
