0d3ec29f4838cad095a10085595ffd0d4d1d6193
afuous <benheller42@gmail.com>
2016-07-31 13:20:54 -0700

    Add delay to test to make it pass
    The caching should probably be changed so that no delay is necessary and each update is atomic

--- test/groups.js
+++ test/groups.js
@@ -5,6 +5,7 @@ let coroutine = Promise.coroutine;
 let assert = require("chai").assert;
 let sessions = require("./shared").sessions;
 let data = require("./shared").data;
+let delay = require("./delay");
 
 describe("groups", function() {
 
@@ -14,6 +15,7 @@ describe("groups", function() {
                 newPosition: "mentor",
             }
         );
+        yield delay(50);
         let groups = yield sessions[1]("GET", "/groups");
         assert.ok(groups.some(g => g.position === "mentor"),
             "user added to mentor PositionGroup"
