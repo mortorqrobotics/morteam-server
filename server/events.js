5c26e7f16bb47899fb8c65cac6183251fb7429e2
afuous <benheller42@gmail.com>
2016-10-15 09:16:33 -0700

    Sort events by date for the client

--- server/events.js
+++ server/events.js
@@ -39,7 +39,7 @@ module.exports = function(imports) {
                 },
                 audienceQuery(req.user),
             ],
-        });
+        }).sort("date");
 
         res.json(events);
 
