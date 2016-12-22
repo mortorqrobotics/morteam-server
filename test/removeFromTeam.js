4495961860bf5097fe0a03dad945820d603cc2a5
AriellaS <ariella.simoni@gmail.com>
2016-12-22 04:53:44 -0800

    Add tests for removing user from team

--- test/removeFromTeam.js
+++ test/removeFromTeam.js
@@ -26,13 +26,45 @@ describe("removing a user from a team", function() {
     }));
 
     before(coroutine(function*() {
-        yield sessions[0]("POST", "/announcements", {
-            content: "stuff",
-            audience: {
-                users: [data.users[0]._id, data.users[1]._id],
-                groups: [],
-            },
-        });
+        yield Promise.all([
+            sessions[0]("POST", "/announcements", {
+                content: "stuff",
+                audience: {
+                    users: [data.users[0]._id, data.users[1]._id],
+                    groups: [],
+                },
+            }),
+            sessions[0]("POST", "/events", {
+                sendEmail: false,
+                name: "thing",
+                description: "stuff",
+                date: new Date(),
+                audience: {
+                    users: [data.users[0]._id, data.users[1]._id],
+                    groups: [],
+                },
+            }),
+            sessions[0]("POST", "/chats", {
+                isTwoPeople: true,
+                otherUser: data.users[1]._id,
+            }),
+            sessions[0]("POST", "/chats", {
+                isTwoPeople: false,
+                name: "thing",
+                audience: {
+                    users: [data.users[1]._id], //user 0 is added as req.user
+                    groups: [],
+                },
+            }),
+            sessions[0]("POST", "/folders", {
+                name: "stuffs",
+                type: "teamFolder",
+                audience: {
+                    users: [data.users[0]._id, data.users[1]._id],
+                    groups: [],
+                },
+            }),
+        ])
     }));
 
     it("should remove the user from announcement hidden groups", coroutine(function*() {
@@ -47,6 +79,56 @@ describe("removing a user from a team", function() {
         );
     }));
 
-    // TODO: add tests for more hidden groups other than just in announcements
+    it("should remove the user from event hidden groups", coroutine(function*() {
+        let date = new Date();
+        let events = yield sessions[0]("GET",
+            `/events/startYear/${date.getFullYear() - 1}/startMonth/0
+                /endYear/${date.getFullYear() + 1}/endMonth/0`);
+        assert.equal(events[0].audience.users.length, 1,
+            "a user was removed from event hidden group"
+        );
+        assert.equal(events[0].audience.users[0], data.users[0]._id,
+            "the correct user was removed from event hidden group"
+        );
+    }));
+
+    it("should remove the user from group chat hidden groups", coroutine(function*() {
+        let chats = yield sessions[0]("GET", "/chats");
+        let groupChat = chats.find(chat => !chat.isTwoPeople);
+        assert.equal(groupChat.audience.users.length, 1,
+            "a user was removed from group chat hidden group"
+        );
+        assert.equal(groupChat.audience.users[0]._id, data.users[0]._id,
+            "the correct user was removed from group chat hidden group"
+        );
+    }));
+
+    it("should not remove the user from private chat hidden groups", coroutine(function*() {
+        let chats = yield sessions[0]("GET", "/chats");
+        let privateChat = chats.find(chat => chat.isTwoPeople);
+        assert.equal(privateChat.audience.users.length, 2,
+            "no user was removed from private chat hidden group"
+        );
+    }));
+
+    it("should remove the user from folder hidden groups", coroutine(function*() {
+        let folders = yield sessions[0]("GET", "/folders");
+        let folder = folders.find(folder => !folder.defaultFolder);
+        assert.equal(folder.audience.users.length, 1,
+            "a user was removed from folder hidden group"
+        );
+        assert.equal(folder.audience.users[0], data.users[0]._id,
+            "the correct user was removed from folder hidden group"
+        );
+    }));
+
+    it("should not remove the user from Personal Files folder hidden group", coroutine(function*() {
+        let folders = yield sessions[0]("GET", "/folders");
+        let personalFolder = folders.find(folder =>
+            folder.defaultFolder && folder.name === "Personal Files");
+        assert.equal(personalFolder.audience.users.length, 1,
+            "no user was removed from Personal Files folder hidden group"
+        );
+    }));
 
 });
